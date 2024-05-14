19892a2edbd56441b4614374508e8d877fcf551d
Uma Roy <uma.roy.us@gmail.com>
2024-05-13 21:17:01 -0700

    compiles

--- testing/zkvm-script/src/main.rs
+++ testing/zkvm-script/src/main.rs
@@ -1,52 +1,168 @@
 //! A simple script that has takes in a block & RPC, fetches the block.
 
+use crate::provider_db::RpcDb;
 use alloy_provider::{Provider, ReqwestProvider};
-use alloy_rpc_types::{BlockId, EIP1186AccountProofResponse};
+use alloy_rpc_types::{Block, BlockId, EIP1186AccountProofResponse};
+use eyre::Ok;
 use reth_revm::database::StateProviderDatabase;
-use revm::db::InMemoryDB;
+use revm::{db::InMemoryDB, Database};
 use url::Url;
+mod provider_db;
+use reth_evm::execute::{BlockExecutionOutput, BlockExecutorProvider, Executor};
+use reth_interfaces::executor::BlockValidationError;
+use reth_node_ethereum::EthereumNode;
+use reth_primitives::{Block as RethBlock, ChainSpecBuilder, Receipts, B256, MAINNET};
+use reth_provider::{BundleStateWithReceipts, ProviderError};
+use revm_primitives::{AccountInfo, HashMap, U256};
 
-struct RpcDb(ReqwestProvider);
+pub struct SP1Input {
+    // The block that will be executed inside the zkVM.
+    pub block: RethBlock,
+    // Used for initializing the InMemoryDB inside the zkVM program.
+    pub merkle_proofs: Vec<EIP1186AccountProofResponse>,
+    // Code
+    pub code: Vec<Vec<u8>>,
+}
 
-// impl StateProvider for RpcDb {
-//     fn storage(
-//         &self,
-//         account: Address,
-//         storage_key: StorageKey,
-//     ) -> ProviderResult<Option<StorageValue>> {
-//     }
+struct WitnessDb {
+    inner: InMemoryDB,
+    state_root: B256,
+}
 
-//     /// Get account code by its hash
-//     fn bytecode_by_hash(&self, code_hash: B256) -> ProviderResult<Option<Bytecode>>;
+impl WitnessDb {
+    fn new(state_root: B256, merkle_proofs: Vec<EIP1186AccountProofResponse>) -> Self {
+        let mut inner = InMemoryDB::default();
+        for proof in merkle_proofs {
+            // TODO: verify proof against state_root
+            // Note that we do not need to hash the code against the code_hash, since
+            // `insert_contract` already does this.
+            let address = proof.address;
+            let account_info = AccountInfo {
+                nonce: proof.nonce.as_limbs()[0], // Is there a better way to do U64 -> u64?
+                balance: proof.balance,
+                code_hash: proof.code_hash,
+                code: None, // TODO: fill this with code
+            };
+            inner.insert_account_info(address, account_info);
+            for storage_proof in proof.storage_proof {
+                // TODO: verify storage proof.
+                let slot = storage_proof.key.0;
+                let value = storage_proof.value;
+                let _ = inner.insert_account_storage(address, slot.into(), value);
+            }
+        }
+        Self { inner, state_root }
+    }
+}
 
-//     /// Get account and storage proofs.
-//     fn proof(&self, address: Address, keys: &[B256]) -> ProviderResult<AccountProof>;
-// }
+// TODO: is there some automatic way to implement this?
+// TODO: fix all the dummy ProviderError
+impl Database for WitnessDb {
+    type Error = ProviderError;
 
-async fn execute() {
-    let block = 1000u64;
+    fn basic(
+        &mut self,
+        address: revm_primitives::Address,
+    ) -> Result<Option<AccountInfo>, Self::Error> {
+        self.inner.basic(address).map_err(|_| ProviderError::UnsupportedProvider)
+    }
+
+    fn code_by_hash(&mut self, code_hash: B256) -> Result<revm_primitives::Bytecode, Self::Error> {
+        self.inner.code_by_hash(code_hash).map_err(|_| ProviderError::UnsupportedProvider)
+    }
+
+    fn storage(
+        &mut self,
+        address: revm_primitives::Address,
+        index: revm_primitives::U256,
+    ) -> Result<revm_primitives::U256, Self::Error> {
+        self.inner.storage(address, index).map_err(|_| ProviderError::UnsupportedProvider)
+    }
+
+    fn block_hash(&mut self, number: revm_primitives::U256) -> Result<B256, Self::Error> {
+        self.inner.block_hash(number).map_err(|_| ProviderError::UnsupportedProvider)
+    }
+}
+
+async fn execute() -> eyre::Result<()> {
+    let block_number = 1000u64;
     let rpc_url = Url::parse("https://example.net").expect("Invalid RPC URL");
+    let merkle_block_td = U256::from(0);
 
     // Initialize a provider.
     let provider = ReqwestProvider::new_http(rpc_url);
 
-    let header =
-        provider.get_block_by_number(block.into(), false).await.expect("Couldn't fetch block");
+    let alloy_block = provider
+        .get_block_by_number(block_number.into(), false)
+        .await?
+        .ok_or(eyre::eyre!("block not found"))?;
 
-    // Initialize a "provider" DB, where `Database` is implemented with a provider.
-    // TODO: figure out how to keep track of what accounts are touched and fetch merkle proofs for
-    // them on the fly.
-    StateProviderDatabase::new()
+    let block = RethBlock::try_from(alloy_block)?;
 
-    // // Inside the zkVM we will have
-    // let state_root = ""; // TODO: read in the state root
-    // let merkle_proofs = vec![]; // TODO: read in the merkle proofs
-    // let db = WitnessDb::new(state_root, merkle_proofs);
-    // let executor =
-    //     reth_node_ethereum::EthExecutorProvider::ethereum(ChainSpec::Ethereum).executor(db);
-    // executor.execute();
+    let chain_spec = ChainSpecBuilder::default()
+        .chain(MAINNET.chain)
+        .genesis(
+            serde_json::from_str(include_str!(
+                "../../../crates/ethereum/node/tests/assets/genesis.json"
+            ))
+            .unwrap(),
+        )
+        .cancun_activated()
+        .build();
+    let provider_db = RpcDb { provider: provider.clone(), block: block_number.into() };
+    let db = StateProviderDatabase::new(provider_db);
+    let executor =
+        reth_node_ethereum::EthExecutorProvider::ethereum(chain_spec.clone().into()).executor(db);
+    let BlockExecutionOutput { state, receipts, .. } = executor.execute(
+        (
+            &block
+                .clone()
+                .with_recovered_senders()
+                .ok_or(BlockValidationError::SenderRecoveryError)?,
+            (merkle_block_td + block.header.difficulty).into(),
+        )
+            .into(),
+    )?;
+    let block_state = BundleStateWithReceipts::new(
+        state,
+        Receipts::from_block_receipt(receipts),
+        block.header.number,
+    );
+
+    // TODO: how do we compute the new state root here? Is there a way to do this incrementally?
+    // // Unpacked `BundleState::state_root_slow` function
+    // let (in_memory_state_root, in_memory_updates) =
+    //     block_state.hash_state_slow().state_root_with_updates(provider.tx_ref())?;
+
+    let next_block = provider
+        .get_block_by_number((block_number + 1).into(), false)
+        .await?
+        .ok_or(eyre::eyre!("next_block not found"))?;
+
+    // Now this is the program inside the zkVM.
+    let sp1_input = SP1Input { block: block.clone(), merkle_proofs: vec![], code: vec![] };
+    let witness_db = WitnessDb::new(sp1_input.block.header.state_root, sp1_input.merkle_proofs);
+    let executor = reth_node_ethereum::EthExecutorProvider::ethereum(chain_spec.clone().into())
+        .executor(witness_db);
+    let BlockExecutionOutput { state, receipts, .. } = executor.execute(
+        (
+            &block
+                .clone()
+                .with_recovered_senders()
+                .ok_or(BlockValidationError::SenderRecoveryError)?,
+            (merkle_block_td + block.header.difficulty).into(),
+        )
+            .into(),
+    )?;
+    let block_state = BundleStateWithReceipts::new(
+        state,
+        Receipts::from_block_receipt(receipts),
+        block.header.number,
+    );
 
     // verify_against_header(db, finalized_state_root);
+
+    Ok(())
 }
 
 fn main() {}
---
+++ testing/zkvm-script/src/provider_db.rs
@@ -0,0 +1,70 @@
+use alloy_provider::{Provider, ReqwestProvider};
+use reth_interfaces::provider::ProviderResult;
+use reth_primitives::{
+    trie::AccountProof, Account, Address, BlockHash, BlockId, BlockNumHash, BlockNumber,
+    BlockNumberOrTag, Bytecode, StorageKey, StorageValue, B256, KECCAK_EMPTY, U256,
+};
+use reth_provider::{
+    AccountReader, BlockHashReader, BlockReader, StateProvider, StateRootProvider,
+};
+use reth_trie::updates::TrieUpdates;
+
+pub struct RpcDb {
+    pub provider: ReqwestProvider, // TODO: make this a "Provider"
+    pub block: BlockId,
+}
+
+impl StateProvider for RpcDb {
+    fn storage(
+        &self,
+        account: Address,
+        storage_key: StorageKey,
+    ) -> ProviderResult<Option<StorageValue>> {
+        todo!();
+    }
+
+    /// Get account code by its hash
+    fn bytecode_by_hash(&self, code_hash: B256) -> ProviderResult<Option<Bytecode>> {
+        unimplemented!();
+    }
+
+    /// Get account and storage proofs.
+    fn proof(&self, address: Address, keys: &[B256]) -> ProviderResult<AccountProof> {
+        todo!();
+    }
+}
+
+impl BlockHashReader for RpcDb {
+    fn block_hash(&self, number: BlockNumber) -> ProviderResult<Option<B256>> {
+        todo!();
+    }
+
+    fn canonical_hashes_range(
+        &self,
+        start: BlockNumber,
+        end: BlockNumber,
+    ) -> reth_interfaces::provider::ProviderResult<Vec<B256>> {
+        todo!();
+    }
+}
+
+impl AccountReader for RpcDb {
+    fn basic_account(&self, address: Address) -> ProviderResult<Option<Account>> {
+        todo!();
+    }
+}
+
+impl StateRootProvider for RpcDb {
+    fn state_root(&self, bundle_state: &revm::db::BundleState) -> ProviderResult<B256> {
+        todo!();
+    }
+
+    fn state_root_with_updates(
+        &self,
+        bundle_state: &revm::db::BundleState,
+    ) -> reth_interfaces::provider::ProviderResult<(B256, TrieUpdates)> {
+        todo!();
+    }
+}
+
+// TODO: implement BlockReader for RpcDb
