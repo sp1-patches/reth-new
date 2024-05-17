ce83aae70744674ebb767378db7495c661d21827
Uma Roy <uma.roy.us@gmail.com>
2024-05-16 21:56:34 -0700

    cleanup

--- testing/zkvm-script/src/main.rs
+++ testing/zkvm-script/src/main.rs
@@ -5,13 +5,44 @@ mod witness;
 use crate::{provider_db::RpcDb, witness::WitnessDb};
 
 use alloy_rpc_types::{Block, BlockId, EIP1186AccountProofResponse};
+use ethers_providers::{Http, Middleware, Provider};
 use eyre::Ok;
 use reth_evm::execute::{BlockExecutionOutput, BlockExecutorProvider, Executor};
 use reth_interfaces::executor::BlockValidationError;
-use reth_primitives::{Block as RethBlock, ChainSpecBuilder, Receipts, B256, MAINNET};
+use reth_primitives::{
+    trie::{AccountProof, StorageProof},
+    Account, Block as RethBlock, ChainSpecBuilder, Receipts, B256, MAINNET,
+};
 use reth_provider::{BundleStateWithReceipts, ProviderError};
+use revm::db::CacheDB;
 use revm_primitives::U256;
 use url::Url;
+
+fn convert_proof(proof: EIP1186AccountProofResponse) -> AccountProof {
+    let address = proof.address;
+    let balance = proof.balance;
+    let code_hash = proof.code_hash;
+    let nonce = proof.nonce.as_limbs()[0];
+    let storage_hash = proof.storage_hash;
+    let account_proof = proof.account_proof;
+    let account_info = Account { nonce, balance, bytecode_hash: code_hash.into() };
+    let storage_proofs = proof.storage_proof.into_iter().map(|storage_proof| {
+        let key = storage_proof.key;
+        let value = storage_proof.value;
+        let proof = storage_proof.proof;
+        let mut sp = StorageProof::new(key.0.into());
+        sp.set_value(value);
+        sp.set_proof(proof);
+        sp
+    });
+    AccountProof {
+        address,
+        info: Some(account_info),
+        proof: account_proof,
+        storage_root: storage_hash.into(),
+        storage_proofs: storage_proofs.collect(),
+    }
+}
 
 /// A struct that holds the input for a zkVM program to execute a block.
 pub struct SP1Input {
@@ -25,20 +56,29 @@ pub struct SP1Input {
 
 async fn get_input(block_number: u64, rpc_url: Url) -> eyre::Result<SP1Input> {
     // We put imports here that are not used in the zkVM program.
-    use alloy_provider::{Provider, ReqwestProvider};
+    use alloy_provider::{Provider as AlloyProvider, ReqwestProvider};
     use reth_revm::database::StateProviderDatabase;
 
-    let merkle_block_td = U256::from(0);
-
     // Initialize a provider.
+    println!("Initializing provider with URL: {}", rpc_url);
+    let ethers_provider = Provider::<Http>::try_from(
+        "https://eth-mainnet.g.alchemy.com/v2/hIxcf_hqT9It2hS8iCFeHKklL8tNyXNF",
+    )
+    .expect("could not instantiate HTTP Provider");
+
     let provider = ReqwestProvider::new_http(rpc_url);
+    let merkle_block_td = U256::ZERO;
+    // provider.header_td_by_number(block_number)?.unwrap_or_default();
 
     let alloy_block = provider
-        .get_block_by_number(block_number.into(), false)
+        .get_block_by_number(block_number.into(), true)
         .await?
         .ok_or(eyre::eyre!("block not found"))?;
 
     let block = RethBlock::try_from(alloy_block)?;
+    for transaction in &block.body {
+        println!("Transaction: {:?}", transaction);
+    }
 
     let chain_spec = ChainSpecBuilder::default()
         .chain(MAINNET.chain)
@@ -48,10 +88,11 @@ async fn get_input(block_number: u64, rpc_url: Url) -> eyre::Result<SP1Input> {
             ))
             .unwrap(),
         )
-        .cancun_activated()
+        .shanghai_activated()
         .build();
-    let provider_db = RpcDb { provider: provider.clone(), block: block_number.into() };
-    let db = StateProviderDatabase::new(provider_db);
+    let provider_db = RpcDb::new(provider.clone(), (block_number - 1).into());
+    let db = CacheDB::new(provider_db);
+    println!("Instantiating executor");
     let executor =
         reth_node_ethereum::EthExecutorProvider::ethereum(chain_spec.clone().into()).executor(db);
     let BlockExecutionOutput { state, receipts, .. } = executor.execute(
@@ -127,8 +168,11 @@ fn verify_stf(sp1_input: SP1Input) -> eyre::Result<()> {
 
 #[tokio::main]
 async fn main() {
-    let block_number = 1000u64;
-    let rpc_url = Url::parse("https://example.net").expect("Invalid RPC URL");
+    let block_number = 18884920u64;
+    let rpc_url =
+        Url::parse("https://eth-mainnet.g.alchemy.com/v2/hIxcf_hqT9It2hS8iCFeHKklL8tNyXNF")
+            .expect("Invalid RPC URL");
+    println!("Fetching block number {} from {}", block_number, rpc_url);
     // Get the input.
     let sp1_input = get_input(block_number, rpc_url).await.expect("Failed to get input");
     // Verify the STF.
--- testing/zkvm-script/src/provider_db.rs
+++ testing/zkvm-script/src/provider_db.rs
@@ -1,68 +1,98 @@
+use core::hash;
+
+use crate::convert_proof;
 #[allow(unused_imports)]
 use alloy_provider::{Provider, ReqwestProvider};
+use alloy_rpc_types::serde_helpers::num;
 use reth_interfaces::provider::ProviderResult;
 use reth_primitives::{
-    trie::AccountProof, Account, Address, BlockId, BlockNumber, Bytecode, StorageKey, StorageValue,
-    B256,
+    trie::AccountProof, Account, Address, BlockId, BlockNumber, StorageKey, StorageValue, B256,
+    U256, U64,
 };
-use reth_provider::{AccountReader, BlockHashReader, StateProvider, StateRootProvider};
+use reth_provider::{
+    AccountReader, BlockHashReader, ProviderError, StateProvider, StateRootProvider,
+};
 use reth_trie::updates::TrieUpdates;
+use revm::DatabaseRef;
+use revm_primitives::{result, AccountInfo, Bytecode, HashMap};
+use tokio::{runtime::Handle, task::block_in_place};
 
 pub struct RpcDb {
     // TODO: make this a general "Provider" trait from alloy_provider.
     pub provider: ReqwestProvider,
     pub block: BlockId,
+    pub handle: Handle,
 }
 
-impl StateProvider for RpcDb {
-    fn storage(
-        &self,
-        account: Address,
-        storage_key: StorageKey,
-    ) -> ProviderResult<Option<StorageValue>> {
-        todo!();
-    }
-
-    /// Get account code by its hash
-    fn bytecode_by_hash(&self, code_hash: B256) -> ProviderResult<Option<Bytecode>> {
-        unimplemented!();
-    }
-
-    /// Get account and storage proofs.
-    fn proof(&self, address: Address, keys: &[B256]) -> ProviderResult<AccountProof> {
-        todo!();
+impl RpcDb {
+    pub fn new(provider: ReqwestProvider, block: BlockId) -> Self {
+        let handle = Handle::current();
+        RpcDb { provider, block, handle }
     }
 }
 
-impl BlockHashReader for RpcDb {
-    fn block_hash(&self, number: BlockNumber) -> ProviderResult<Option<B256>> {
-        todo!();
+impl DatabaseRef for RpcDb {
+    type Error = ProviderError;
+
+    fn basic_ref(&self, address: Address) -> Result<Option<AccountInfo>, Self::Error> {
+        println!("getting account {}", address);
+        let result = block_in_place(|| {
+            Handle::current()
+                .block_on(async move { self.provider.get_proof(address, vec![], self.block).await })
+        });
+        let code = block_in_place(|| {
+            Handle::current()
+                .block_on(async move { self.provider.get_code_at(address, self.block).await })
+        });
+        result
+            .map(|proof| {
+                println!("address {}, nonce: {}", address, proof.nonce.as_limbs()[0]);
+                Some(AccountInfo {
+                    nonce: proof.nonce.as_limbs()[0],
+                    balance: proof.balance,
+                    code_hash: proof.code_hash,
+                    code: code.ok().map(|code| Bytecode::new_raw(code)),
+                })
+            })
+            .map_err(|err| ProviderError::FsPathError("hi".into()))
     }
 
-    fn canonical_hashes_range(
-        &self,
-        start: BlockNumber,
-        end: BlockNumber,
-    ) -> reth_interfaces::provider::ProviderResult<Vec<B256>> {
-        todo!();
-    }
-}
-
-impl AccountReader for RpcDb {
-    fn basic_account(&self, address: Address) -> ProviderResult<Option<Account>> {
-        todo!();
-    }
-}
-
-impl StateRootProvider for RpcDb {
-    fn state_root(&self, bundle_state: &revm::db::BundleState) -> ProviderResult<B256> {
-        todo!();
+    fn code_by_hash_ref(&self, code_hash: B256) -> Result<Bytecode, Self::Error> {
+        // We use basic_ref to get the code from the provider.
+        unimplemented!()
     }
 
-    fn state_root_with_updates(
-        &self,
-        bundle_state: &revm::db::BundleState,
-    ) -> reth_interfaces::provider::ProviderResult<(B256, TrieUpdates)> {
-        todo!();
+    fn storage_ref(&self, address: Address, index: U256) -> Result<U256, Self::Error> {
+        println!("getting address {} {}", address, index);
+        let result = block_in_place(|| {
+            Handle::current().block_on(async move {
+                // TODO: this index might need to be hashed, not sure.
+                self.provider.get_storage_at(address, index.into(), self.block).await
+            })
+        });
+        result.map(|value| value.into()).map_err(|err| ProviderError::FsPathError("hi".into()))
+    }
+
+    fn block_hash_ref(&self, number: U256) -> Result<B256, Self::Error> {
+        println!("getting block_hash {:?}", number);
+        let num_u64 = number.as_limbs()[0];
+        let result = block_in_place(|| {
+            Handle::current().block_on(async move {
+                self.provider.get_block_by_number(num_u64.into(), false).await
+            })
+        });
+        if result.is_err() {
+            return Err(ProviderError::FsPathError("hi".into()));
+        }
+        let block = result.unwrap();
+        if block.is_none() {
+            return Err(ProviderError::FsPathError("hi".into()));
+        }
+        let block = block.unwrap();
+        let hash = block.header.hash;
+        if hash.is_none() {
+            return Err(ProviderError::FsPathError("hi".into()));
+        }
+        Ok(hash.unwrap().into())
     }
 }
